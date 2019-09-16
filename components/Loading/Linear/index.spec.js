6b831989d2acf0d7121a682e73afc416aa2e2668
Tesar Wijaya <tesarwijaya@Tesars-MacBook-Pro.local>
2019-09-16 17:31:28 +0700

    [ADD] component loading linear test

---
+++ components/Loading/Linear/index.spec.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { createMount } from '@material-ui/core/test-utils'
+
+import {
+  LinearProgress,
+} from '@material-ui/core'
+
+import { Linear } from './index'
+
+describe('Components/Loading/Linear', () => {
+  it('should have LinearProgress component', () => {
+    const mount = createMount()
+    const wrapper = mount(<Linear><div id="test" /></Linear>)
+
+    expect(wrapper.find(LinearProgress).length).toEqual(1)
+  })
+
+  it('should render child component', () => {
+    const mount = createMount()
+    const wrapper = mount(<Linear><div id="test" /></Linear>)
+
+    expect(wrapper.children().containsMatchingElement(<div id="test" />)).toEqual(true)
+  })
+})
