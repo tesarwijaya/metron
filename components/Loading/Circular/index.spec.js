690e04a4d4105b497361247f6e5326dd99d3eecb
Tesar Wijaya <tesarwijaya@Tesars-MacBook-Pro.local>
2019-09-16 17:30:20 +0700

    [ADD] component loading circular test

---
+++ components/Loading/Circular/index.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createMount } from '@material-ui/core/test-utils'
+
+import {
+  CircularProgress,
+} from '@material-ui/core'
+
+import { Circular } from './index'
+
+describe('Components/Loading/Circular', () => {
+  it('should have CircularProgress component', () => {
+    const mount = createMount()
+    const wrapper = mount(<Circular />)
+
+    expect(wrapper.find(CircularProgress).length).toEqual(1)
+  })
+})
