6f5205f82b1e0de9a4d2682bfded73ca2fcb35cb
Tesar Wijaya <tesarwijaya@Tesars-MacBook-Pro.local>
2019-09-16 16:04:05 +0700

    [ADD] container item test

---
+++ components/Container/Item/index.spec.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { createMount } from '@material-ui/core/test-utils'
+
+import {
+  Grid,
+} from '@material-ui/core'
+
+import { Item } from './index'
+
+describe('Components/Container/Item', () => {
+  it('should have Grid item component', () => {
+    const mount = createMount()
+    const wrapper = mount(<Item><div /></Item>)
+
+    expect(wrapper.find(Grid).props().item).toEqual(true)
+  })
+
+  it('should render children component', () => {
+    const mount = createMount()
+    const wrapper = mount(<Item><div /></Item>)
+
+    expect(wrapper.find(Grid).children().containsMatchingElement(<div />)).toEqual(true)
+  })
+})
